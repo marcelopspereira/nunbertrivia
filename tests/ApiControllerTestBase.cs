465cd4d6cd39640541782e5da0d902ee83ca8c55
Marcelo Pereira da Silva <marcelopspereira@gmail.com>
2018-09-09 21:20:42 -0300

    Add ApiControllerTestBase.cs Add Reference Api Add package TestHost e Hosting Server

---
+++ tests/ApiControllerTestBase.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Net.Http;
+using api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace tests
+{
+    public class ApiControllerTestBase
+    {
+        protected HttpClient GetClient()
+        {
+            var builder = new WebHostBuilder()
+                   .UseContentRoot(Directory.GetCurrentDirectory())
+                   .UseStartup<Startup>()
+                   .UseEnvironment("Testing");
+
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+
+            //client  always expects json results
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+            {
+
+            }
+        }
+    }
+}
